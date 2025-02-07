368aa10b3762adef054cfe1f364d14ede97013ab
AhasnaCharumee <charumeeahasna@gmail.com>
2025-02-07 10:54:33 +0530

    Introduse hibernate and run project show database in mysql

--- src/main/java/lk/ijse/gdse72/Main.java
+++ src/main/java/lk/ijse/gdse72/Main.java
@@ -1,7 +1,25 @@
 package lk.ijse.gdse72;
 
+import config.FactoryConfiguration;
+import entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Customer customer = new Customer(
+                1,
+                "Saman",
+                "saman@gmail.com"
+        );
+        Session session = FactoryConfiguration.getInstance().getSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.save(customer);
+
+        transaction.commit();
+        session.close();
+
     }
 }
