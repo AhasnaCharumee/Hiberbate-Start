c88103725950767466c11c27c427d0bfb68ccd18
AhasnaCharumee <charumeeahasna@gmail.com>
2025-02-07 10:17:50 +0530

    create config package and create class factoryconfiguration to boostrap xml

--- src/main/java/entity/Customer.java
+++ src/main/java/entity/Customer.java
@@ -1,4 +1,22 @@
 package entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity//map to table
+@Table(name = "customer_table")
 public class Customer {
+    @Id//primary key
+    private int id;
+    private String name;
+    private String email;
+
+
 }
